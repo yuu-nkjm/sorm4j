ed3b4eec00e0d0f3a164bee4494a8e9cf7bcce38
Yuu Nakajima <yuu.nkjm.pub@gmail.com>
2021-02-25 21:40:10 +0900

    Add ConnectionSourceTest.java

---
+++ src/test/java/org/nkjmlab/sorm4j/connectionsource/ConnectionSourceTest.java
@@ -0,0 +1,25 @@
+package org.nkjmlab.sorm4j.connectionsource;
+
+import java.sql.SQLException;
+import org.h2.jdbcx.JdbcConnectionPool;
+import org.junit.jupiter.api.Test;
+
+class ConnectionSourceTest {
+
+  String url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;";
+  String user = "sa";
+  String pw = "";
+
+  @Test
+  void test() {
+    JdbcConnectionPool cp = JdbcConnectionPool.create(url, user, pw);
+    DataSourceConnectionSource ds = new DataSourceConnectionSource(cp);
+    org.assertj.core.api.Assertions.assertThat(ds.getDataSource()).isEqualTo(cp);
+    try {
+      org.assertj.core.api.Assertions.assertThat(ds.getConnection()).isNotNull();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
