6efef0fe4ae03534a6755838a0012d989c8b1f65
Yuu Nakajima <yuu.nkjm.pub@gmail.com>
2021-03-01 11:14:08 +0900

    Add Java doc for LazyResultSet.java

--- src/main/java/org/nkjmlab/sorm4j/LazyResultSet.java
+++ src/main/java/org/nkjmlab/sorm4j/LazyResultSet.java
@@ -15,7 +15,11 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import org.nkjmlab.sorm4j.mapping.AbstractOrmMapper;
 
-
+/**
+ * Represents a result set from database.
+ *
+ * @param <T> element type in result
+ */
 public final class LazyResultSet<T> implements Iterable<T>, Closeable, AutoCloseable {
 
   private final Class<T> objectClass;
@@ -31,35 +35,63 @@ public final class LazyResultSet<T> implements Iterable<T>, Closeable, AutoClose
     this.resultSet = resultSet;
   }
 
+  /**
+   * Returns the row in the result set and close. If the row is not unique, Exception is thrown.
+   *
+   * @return
+   */
   public T one() {
     T ret = ormMapper.loadOne(objectClass, resultSet);
     close();
     return ret;
   }
 
+  /**
+   * Returns the first row in the result set and close.
+   *
+   * @return
+   */
   public T first() {
     T ret = ormMapper.loadFirst(objectClass, resultSet);
     close();
     return ret;
   }
 
+  /**
+   * Returns results in a List.
+   *
+   * @return
+   */
   public List<T> toList() {
     List<T> ret = ormMapper.loadPojoList(objectClass, resultSet);
     close();
     return ret;
   }
 
+  /**
+   * Returns results in a List of {@code Map<String, Object>}.
+   *
+   * @return
+   */
   public List<Map<String, Object>> toMapList() {
     List<Map<String, Object>> ret = ormMapper.loadMapList(resultSet);
     close();
     return ret;
   }
 
+  /**
+   * Iterates all the rows of the result set. The Iterator must be closed to release database
+   * resources. The iterator is closed automatically if hasNext is false.
+   */
   @Override
   public Iterator<T> iterator() {
     return new LazyResultSetIterator<>(ormMapper, objectClass, stmt, resultSet);
   }
 
+  /**
+   * Streams all the rows of the result set. The stream must be closed to release database
+   * resources.
+   */
   public Stream<T> stream() {
     return StreamSupport.stream(spliteratorUnknownSize(iterator(), ORDERED), false);
   }
