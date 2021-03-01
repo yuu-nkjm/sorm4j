a7af4e6dc48dba13b1196dbdab8b47329932294c
Yuu Nakajima <yuu.nkjm.pub@gmail.com>
2021-03-01 09:30:55 +0900

    Change log level

--- src/main/java/org/nkjmlab/sorm4j/mapping/Mapping.java
+++ src/main/java/org/nkjmlab/sorm4j/mapping/Mapping.java
@@ -78,14 +78,12 @@ abstract class Mapping<T> {
         s = s != null ? g : isValidSetter(setters.get(fieldName));
       }
       if (f == null && (g == null || s == null)) {
-        log.warn(
+        log.debug(
             "Skip matching with Column [{}] to field because could not found corresponding field.",
             column);
       } else {
         ret.put(column.getName(), new Accessor(column, f, g, s));
       }
-
-
     }
     return ret;
   }
