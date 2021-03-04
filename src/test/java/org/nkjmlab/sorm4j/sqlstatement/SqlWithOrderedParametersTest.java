38ffc130603242687fed4a01ade95891e66a2b38
Yuu Nakajima <yuu.nkjm.pub@gmail.com>
2021-03-04 19:05:55 +0900

    Fix bug of SqlWithOrderedParameters.

--- src/test/java/org/nkjmlab/sorm4j/sqlstatement/SqlWithOrderedParametersTest.java
+++ src/test/java/org/nkjmlab/sorm4j/sqlstatement/SqlWithOrderedParametersTest.java
@@ -8,9 +8,19 @@ class SqlWithOrderedParametersTest {
 
   @Test
   void testToSqlStatementStringObjectArray() {
-    assertThat(
-        SqlWithOrderedParameters.toSqlStatement("select * from a where in(?)", List.of("alice", 1)).toString())
-            .contains("[select * from a where in(?)] with [['alice', 1]]");
+    SqlStatement sqlSt =
+        SqlWithOrderedParameters.toSqlStatement("select * from a where in(?)", List.of("alice", 1));
+    assertThat(sqlSt.getParameters().length).isEqualTo(1);
+    assertThat(sqlSt.getParameters()[0]).isEqualTo("'alice', 1");
+
   }
 
+  @Test
+  void testToSqlStatementStringObject() {
+    SqlStatement sqlSt =
+        SqlWithOrderedParameters.toSqlStatement("select * from a where id=? and id=?", 1, 2);
+    assertThat(sqlSt.getParameters().length).isEqualTo(2);
+    assertThat(sqlSt.getParameters()[0]).isEqualTo(1);
+    assertThat(sqlSt.getParameters()[1]).isEqualTo(2);
+  }
 }
