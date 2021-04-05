6c07933e7ba39a2d0e94e3782d58f935406292e4
Yuu Nakajima <yuu.nkjm.pub@gmail.com>
2021-04-05 10:27:43 +0900

    Add Tuple2 and Tuple3

---
+++ src/main/java/org/nkjmlab/sorm4j/sql/tuple/Tuples.java
@@ -0,0 +1,24 @@
+package org.nkjmlab.sorm4j.sql.tuple;
+
+import org.nkjmlab.sorm4j.annotation.Experimental;
+
+/**
+ * Represents a tuple of object, which typically represents joined two rows.
+ *
+ * @author nkjm
+ *
+ */
+@Experimental
+public final class Tuples {
+  private Tuples() {}
+
+  public static <T1, T2> Tuple2<T1, T2> of(T1 t1, T2 t2) {
+    return new Tuple2<T1, T2>(t1, t2);
+  }
+
+
+  public static <T1, T2, T3> Tuple3<T1, T2, T3> of(T1 t1, T2 t2, T3 t3) {
+    return new Tuple3<T1, T2, T3>(t1, t2, t3);
+  }
+
+}
