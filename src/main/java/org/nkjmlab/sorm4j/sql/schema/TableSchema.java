a1ab70e21c1067ad01b21bce6b088dcb7d0b905c
Yuu Nakajima <yuu.nkjm.pub@gmail.com>
2021-04-30 22:42:47 +0900

    Add TableSchema.Builder

--- src/main/java/org/nkjmlab/sorm4j/sql/schema/TableSchema.java
+++ src/main/java/org/nkjmlab/sorm4j/sql/schema/TableSchema.java
@@ -12,152 +12,196 @@ import org.nkjmlab.sorm4j.annotation.Experimental;
 public class TableSchema {
 
   private final String tableName;
-  private final Map<String, String[]> columnDefinitions;
-  private final List<String[]> uniqueColumnPairs;
-  private String[] primaryKeys;
+  private final String tableSchema;
+  private final List<String> columnNames;
+  private final String createTableStatement;
+  private final String dropTableStatement;
+  private final List<String> createIndexStatements;
 
-  private final List<String[]> indexColumns;
+  private TableSchema(Builder builder) {
+    this.tableName = builder.tableName;
+    this.tableSchema = builder.getTableSchema();
+    this.createTableStatement = "create table if not exists " + tableSchema;
+    this.dropTableStatement = "drop table if exists " + tableName;
+    this.columnNames = builder.getColumunNames();
+    this.createIndexStatements = builder.getCreateIndexStatements();
 
-
-  public TableSchema(String tableName) {
-    this.tableName = tableName;
-    this.columnDefinitions = new LinkedHashMap<>();
-    this.uniqueColumnPairs = new ArrayList<>();
-    this.indexColumns = new ArrayList<>();
-  }
-
-  /**
-   * Adds an unique constraint.
-   *
-   * <p>
-   * For example,
-   *
-   * <pre>
-   * addUniqueConstraint("id","name")  converted to "UNIQUE (id, name)"
-   * </pre>
-   *
-   * @param columnName
-   * @param dataTypeAndOptions
-   */
-  public void addColumnDefinition(String columnName, String... dataTypeAndOptions) {
-    columnDefinitions.put(columnName, dataTypeAndOptions);
-  }
-
-  /**
-   * Adds an unique constraint.
-   *
-   * <p>
-   * For example,
-   *
-   * <pre>
-   * addUniqueConstraint("id","name")  converted to "UNIQUE (id, name)"
-   * </pre>
-   *
-   * @param uniqueColumnPair
-   */
-  public void addUniqueConstraint(String... uniqueColumnPair) {
-    uniqueColumnPairs.add(uniqueColumnPair);
-  }
-
-  public List<String> getColumnNames() {
-    return getColumunNames(columnDefinitions);
   }
 
   public String getTableName() {
     return tableName;
   }
 
-  /**
-   * Gets a table schema.
-   *
-   * @return
-   */
-
-  public String getTableSchema() {
-    return getTableSchema(getTableName(), columnDefinitions, primaryKeys, uniqueColumnPairs);
-  }
-
-  /**
-   * Adds a column pair for an index key.
-   *
-   * @param indexColumnPair
-   */
-  public void addIndexColumn(String... indexColumnPair) {
-    indexColumns.add(indexColumnPair);
-  }
-
-
-  /**
-   * Sets attributes as primary key attributes.
-   *
-   * <p>
-   * For example,
-   *
-   * <pre>
-   * setPrimaryKey("id","name")  converted to "PRIMARY KEY (id, name)"
-   * </pre>
-   *
-   * @param attributes
-   */
-  public void setPrimaryKey(String... attributes) {
-    this.primaryKeys = attributes;
-  }
-
-
-
-  private static String createPrimaryKeyConstraint(String[] primaryKeys) {
-    if (primaryKeys == null || primaryKeys.length == 0) {
-      return "";
-    }
-    return ", primary key" + "(" + join(", ", primaryKeys) + ")";
-  }
-
-  private static String createUniqueConstraint(List<String[]> uniqueColumnPairs) {
-    if (uniqueColumnPairs == null || uniqueColumnPairs.size() == 0) {
-      return "";
-    }
-    return ", " + String.join(", ", uniqueColumnPairs.stream()
-        .map(u -> "unique" + "(" + join(", ", u) + ")").toArray(String[]::new));
-  }
-
-
-  private static List<String> getColumunNames(Map<String, String[]> columnDefinitions) {
-    return columnDefinitions.entrySet().stream().map(e -> e.getKey()).collect(Collectors.toList());
-  }
-
-
-  private static String getTableSchema(String tableName, Map<String, String[]> columns,
-      String[] primaryKeys, List<String[]> uniqueColumnPairs) {
-    String schema = tableName + "(" + join(", ", getColumuns(columns))
-        + createPrimaryKeyConstraint(primaryKeys) + createUniqueConstraint(uniqueColumnPairs) + ")";
-    return schema;
-  }
-
-  private static List<String> getColumuns(Map<String, String[]> columnDefinisions) {
-    return columnDefinisions.keySet().stream()
-        .map(columnName -> columnName + " " + join(" ", columnDefinisions.get(columnName)))
-        .collect(Collectors.toList());
-  }
-
-  public String getDropTableStatement() {
-    return "drop table if exists " + tableName;
-  }
-
-  public String getCreateTableStatement() {
-    return "create table if not exists " + getTableSchema();
+  public List<String> getColumnNames() {
+    return columnNames;
   }
 
   public List<String> getCreateIndexStatements() {
-    return indexColumns.stream()
-        .map(columns -> getCreateIndexOnStatement("index_" + tableName + "_" + join("_", columns),
-            tableName, columns))
-        .collect(Collectors.toList());
+    return createIndexStatements;
   }
 
-  private static String getCreateIndexOnStatement(String indexName, String tableName,
-      String... columns) {
-    return "create index if not exists " + indexName + " on " + tableName + "("
-        + String.join(", ", columns) + ")";
+  public String getCreateTableStatement() {
+    return createTableStatement;
   }
+
+  public String getDropTableStatement() {
+    return dropTableStatement;
+  }
+
+
+  public String getTableSchema() {
+    return tableSchema;
+  }
+
+
+  public static class Builder {
+    private String tableName;
+    private final Map<String, String[]> columnDefinitions;
+    private String[] primaryKeys;
+    private final List<String[]> uniqueColumnPairs;
+    private final List<String[]> indexColumns;
+
+    public Builder() {
+      this.columnDefinitions = new LinkedHashMap<>();
+      this.uniqueColumnPairs = new ArrayList<>();
+      this.indexColumns = new ArrayList<>();
+    }
+
+    public TableSchema build() {
+      return new TableSchema(this);
+    }
+
+    public Builder setTableName(String tableName) {
+      this.tableName = tableName;
+      return this;
+    }
+
+    /**
+     * Adds an unique constraint.
+     *
+     * <p>
+     * For example,
+     *
+     * <pre>
+     * addUniqueConstraint("id","name")  converted to "UNIQUE (id, name)"
+     * </pre>
+     *
+     * @param columnName
+     * @param dataTypeAndOptions
+     * @return
+     */
+    public Builder addColumnDefinition(String columnName, String... dataTypeAndOptions) {
+      columnDefinitions.put(columnName, dataTypeAndOptions);
+      return this;
+    }
+
+    /**
+     * Adds a column pair for an index key.
+     *
+     * @param indexColumnPair
+     */
+    public Builder addIndexColumn(String... indexColumnPair) {
+      indexColumns.add(indexColumnPair);
+      return this;
+    }
+
+    /**
+     * Adds an unique constraint.
+     *
+     * <p>
+     * For example,
+     *
+     * <pre>
+     * addUniqueConstraint("id","name")  converted to "UNIQUE (id, name)"
+     * </pre>
+     *
+     * @param uniqueColumnPair
+     */
+    public Builder addUniqueConstraint(String... uniqueColumnPair) {
+      uniqueColumnPairs.add(uniqueColumnPair);
+      return this;
+    }
+
+    /**
+     * Sets attributes as primary key attributes.
+     *
+     * <p>
+     * For example,
+     *
+     * <pre>
+     * setPrimaryKey("id","name")  converted to "PRIMARY KEY (id, name)"
+     * </pre>
+     *
+     * @param attributes
+     */
+    public Builder setPrimaryKey(String... attributes) {
+      this.primaryKeys = attributes;
+      return this;
+    }
+
+    /**
+     * Gets a table schema.
+     *
+     * @return
+     */
+
+    private String getTableSchema() {
+      return getTableSchema(tableName, columnDefinitions, primaryKeys, uniqueColumnPairs);
+    }
+
+    private List<String> getCreateIndexStatements() {
+      return indexColumns.stream()
+          .map(columns -> getCreateIndexOnStatement("index_" + tableName + "_" + join("_", columns),
+              tableName, columns))
+          .collect(Collectors.toList());
+    }
+
+    private List<String> getColumunNames() {
+      return getColumunNames(columnDefinitions);
+    }
+
+    private static String createPrimaryKeyConstraint(String[] primaryKeys) {
+      if (primaryKeys == null || primaryKeys.length == 0) {
+        return "";
+      }
+      return ", primary key" + "(" + join(", ", primaryKeys) + ")";
+    }
+
+    private static String createUniqueConstraint(List<String[]> uniqueColumnPairs) {
+      if (uniqueColumnPairs == null || uniqueColumnPairs.size() == 0) {
+        return "";
+      }
+      return ", " + String.join(", ", uniqueColumnPairs.stream()
+          .map(u -> "unique" + "(" + join(", ", u) + ")").toArray(String[]::new));
+    }
+
+    private static List<String> getColumunNames(Map<String, String[]> columnDefinitions) {
+      return columnDefinitions.entrySet().stream().map(e -> e.getKey())
+          .collect(Collectors.toList());
+    }
+
+    private static List<String> getColumuns(Map<String, String[]> columnDefinisions) {
+      return columnDefinisions.keySet().stream()
+          .map(columnName -> columnName + " " + join(" ", columnDefinisions.get(columnName)))
+          .collect(Collectors.toList());
+    }
+
+    private static String getCreateIndexOnStatement(String indexName, String tableName,
+        String... columns) {
+      return "create index if not exists " + indexName + " on " + tableName + "("
+          + String.join(", ", columns) + ")";
+    }
+
+    private static String getTableSchema(String tableName, Map<String, String[]> columns,
+        String[] primaryKeys, List<String[]> uniqueColumnPairs) {
+      String schema = tableName + "(" + join(", ", getColumuns(columns))
+          + createPrimaryKeyConstraint(primaryKeys) + createUniqueConstraint(uniqueColumnPairs)
+          + ")";
+      return schema;
+    }
+
+  }
+
+
 
 }
--- src/test/java/org/nkjmlab/sorm4j/sql/schema/TableSchemaTest.java
+++ src/test/java/org/nkjmlab/sorm4j/sql/schema/TableSchemaTest.java
@@ -8,20 +8,17 @@ class TableSchemaTest {
 
   private static GuestTable guestTable = new GuestTable();
 
-  private static class GuestTable extends TableSchema {
+  private static class GuestTable {
 
     private static String ID = "id";
     private static String NAME = "name";
     private static String TEL = "tel";
+    private final TableSchema schema;
 
     public GuestTable() {
-      super("guests");
-      addColumnDefinition(ID, INT);
-      addColumnDefinition(NAME, VARCHAR);
-      addColumnDefinition(TEL, VARCHAR);
-      addUniqueConstraint(TEL);
-      addIndexColumn(TEL);
-      setPrimaryKey(ID);
+      this.schema = new TableSchema.Builder().setTableName("guests").addColumnDefinition(ID, INT)
+          .addColumnDefinition(NAME, VARCHAR).addColumnDefinition(TEL, VARCHAR)
+          .addUniqueConstraint(TEL).addIndexColumn(TEL).setPrimaryKey(ID).build();
     }
 
   }
@@ -29,24 +26,24 @@ class TableSchemaTest {
 
   @Test
   void testGetColumnNames() {
-    assertThat(guestTable.getColumnNames()).contains(GuestTable.ID, GuestTable.NAME,
+    assertThat(guestTable.schema.getColumnNames()).contains(GuestTable.ID, GuestTable.NAME,
         GuestTable.TEL);
   }
 
   @Test
   void testGetName() {
-    assertThat(guestTable.getTableName()).isEqualToIgnoringCase("guests");
+    assertThat(guestTable.schema.getTableName()).isEqualToIgnoringCase("guests");
   }
 
   @Test
   void testGetTableSchema() {
-    assertThat(guestTable.getTableSchema()).isEqualToIgnoringCase(
+    assertThat(guestTable.schema.getTableSchema()).isEqualToIgnoringCase(
         "guests(id int, name varchar, tel varchar, primary key(id), unique(tel))");
   }
 
   @Test
   void testGetIndexSchema() {
-    assertThat(guestTable.getCreateIndexStatements().get(0))
+    assertThat(guestTable.schema.getCreateIndexStatements().get(0))
         .isEqualToIgnoringCase("CREATE INDEX IF NOT EXISTS index_guests_tel ON guests(tel)");
   }
 
