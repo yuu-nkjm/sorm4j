5da06610e399bc4dabc519721f6ca5d4bdcbea03
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2022-01-25 11:34:39 +0900

    Refactoring test

---
+++ sorm4j/src/test/java/org/nkjmlab/sorm4j/common/TupleTest.java
@@ -0,0 +1,41 @@
+package org.nkjmlab.sorm4j.common;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.nkjmlab.sorm4j.common.Tuple.Tuple1;
+import org.nkjmlab.sorm4j.common.Tuple.Tuple2;
+import org.nkjmlab.sorm4j.common.Tuple.Tuple3;
+
+class TupleTest {
+
+  @Test
+  void testOfT1() {
+    Tuple1<String> t1 = Tuple.of("t1");
+    Tuple1<String> t2 = Tuple.of("t1");
+
+    assertThat(t1.equals(t2)).isTrue();
+    assertThat(t1.hashCode()).isEqualTo(t2.hashCode());
+    assertThat(t1.toString()).isEqualTo("(t1)");
+  }
+
+  @Test
+  void testOfT2() {
+    Tuple2<String, String> t1 = Tuple.of("t1", "t2");
+    Tuple2<String, String> t2 = Tuple.of("t1", "t2");
+
+    assertThat(t1.equals(t2)).isTrue();
+    assertThat(t1.hashCode()).isEqualTo(t2.hashCode());
+    assertThat(t1.toString()).isEqualTo("(t1, t2)");
+  }
+
+  @Test
+  void testOfT3() {
+    Tuple3<String, String, String> t1 = Tuple.of("t1", "t2", "t3");
+    Tuple3<String, String, String> t2 = Tuple.of("t1", "t2", "t3");
+
+    assertThat(t1.equals(t2)).isTrue();
+    assertThat(t1.hashCode()).isEqualTo(t2.hashCode());
+    assertThat(t1.toString()).isEqualTo("(t1, t2, t3)");
+  }
+
+}
--- sorm4j/src/test/java/org/nkjmlab/sorm4j/context/DefaultSqlParametersSetterTest.java
+++ sorm4j/src/test/java/org/nkjmlab/sorm4j/context/DefaultSqlParametersSetterTest.java
@@ -35,15 +35,91 @@ class DefaultSqlParametersSetterTest {
       assertThat(toSqlArray("boolean", conn, TestUtils.PRIMITIVE_BOOLEAN_ARRAY).getBaseType())
           .isEqualTo(JDBCType.BOOLEAN.getVendorTypeNumber());
 
+      assertThat(toSqlArray("java.lang.Boolean", conn, TestUtils.BOOLEAN_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.BOOLEAN.getVendorTypeNumber());
+
       assertThat(toSqlArray("char", conn, TestUtils.PRIMITIVE_CHAR_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.CHAR.getVendorTypeNumber());
+      assertThat(toSqlArray("java.lang.Character", conn, TestUtils.CHARRACTER_ARRAY).getBaseType())
           .isEqualTo(JDBCType.CHAR.getVendorTypeNumber());
 
       assertThat(toSqlArray("byte", conn, TestUtils.PRIMITIVE_BYTE_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.TINYINT.getVendorTypeNumber());
+      assertThat(toSqlArray("java.lang.Byte", conn, TestUtils.BYTE_ARRAY).getBaseType())
           .isEqualTo(JDBCType.TINYINT.getVendorTypeNumber());
 
       assertThat(toSqlArray("short", conn, TestUtils.PRIMITIVE_SHORT_ARRAY).getBaseType())
           .isEqualTo(JDBCType.SMALLINT.getVendorTypeNumber());
 
+      assertThat(toSqlArray("java.lang.Short", conn, TestUtils.SHORT_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.SMALLINT.getVendorTypeNumber());
+
+      assertThat(toSqlArray("int", conn, TestUtils.PRIMITIVE_INT_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.INTEGER.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.lang.Integer", conn, TestUtils.INTEGER_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.INTEGER.getVendorTypeNumber());
+
+      assertThat(toSqlArray("long", conn, TestUtils.PRIMITIVE_LONG_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.BIGINT.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.lang.Long", conn, TestUtils.LONG_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.BIGINT.getVendorTypeNumber());
+
+
+      assertThat(toSqlArray("float", conn, TestUtils.PRIMITIVE_FLOAT_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.REAL.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.lang.Float", conn, TestUtils.FLOAT_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.REAL.getVendorTypeNumber());
+
+      assertThat(toSqlArray("double", conn, TestUtils.PRIMITIVE_DOUBLE_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.DOUBLE.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.lang.Double", conn, TestUtils.DOUBLE_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.DOUBLE.getVendorTypeNumber());
+
+
+      assertThat(toSqlArray("java.lang.String", conn, TestUtils.STRING_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.VARCHAR.getVendorTypeNumber());
+
+      assertThat(
+          toSqlArray("java.math.BigDecimal", conn, TestUtils.BIG_DECIMAL_ARRAY).getBaseType())
+              .isEqualTo(JDBCType.NUMERIC.getVendorTypeNumber());
+
+
+      assertThat(toSqlArray("java.sql.Date", conn, TestUtils.DATE_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.DATE.getVendorTypeNumber());
+      assertThat(toSqlArray("java.sql.Time", conn, TestUtils.TIME_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.TIME.getVendorTypeNumber());
+      assertThat(toSqlArray("java.sql.Timestamp", conn, TestUtils.TIMESTAMP_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.TIMESTAMP.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.time.Instant", conn, TestUtils.INSTANT_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.TIMESTAMP_WITH_TIMEZONE.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.time.Instant", conn, TestUtils.INSTANT_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.TIMESTAMP_WITH_TIMEZONE.getVendorTypeNumber());
+
+      assertThat(
+          toSqlArray("java.time.OffsetTime", conn, TestUtils.OFFSET_TIME_ARRAY).getBaseType())
+              .isEqualTo(JDBCType.TIME_WITH_TIMEZONE.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.time.OffsetDateTime", conn, TestUtils.OFFSET_DATE_TIME_ARRAY)
+          .getBaseType()).isEqualTo(JDBCType.TIMESTAMP_WITH_TIMEZONE.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.time.LocalDate", conn, TestUtils.LOCAL_DATE_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.DATE.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.time.LocalTime", conn, TestUtils.LOCAL_TIME_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.TIME.getVendorTypeNumber());
+
+      assertThat(toSqlArray("java.time.LocalDateTime", conn, TestUtils.LOCAL_DATE_TIME_ARRAY)
+          .getBaseType()).isEqualTo(JDBCType.TIMESTAMP.getVendorTypeNumber());
+
+
+      assertThat(toSqlArray("java.lang.Object", conn, TestUtils.OBJECT_ARRAY).getBaseType())
+          .isEqualTo(JDBCType.JAVA_OBJECT.getVendorTypeNumber());
 
 
     } catch (SQLException e) {
