4c97e2bc06e98c2e76fba9182a4c33dbd2c5f3d6
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2023-12-15 09:58:38 +0900

    Remove unused method

--- sorm4j/src/main/java/org/nkjmlab/sorm4j/util/h2/H2Table.java
+++ sorm4j/src/main/java/org/nkjmlab/sorm4j/util/h2/H2Table.java
@@ -4,10 +4,7 @@ import java.io.File;
 
 import org.nkjmlab.sorm4j.annotation.Experimental;
 import org.nkjmlab.sorm4j.table.Table;
-import org.nkjmlab.sorm4j.util.h2.commands.ScriptSql;
 import org.nkjmlab.sorm4j.util.h2.functions.system.CsvWrite;
-import org.nkjmlab.sorm4j.util.h2.functions.table.CsvRead;
-import org.nkjmlab.sorm4j.util.h2.grammar.ScriptCompressionEncryption;
 
 @Experimental
 public interface H2Table<T> extends Table<T>, H2Orm {
@@ -26,39 +23,11 @@ public interface H2Table<T> extends Table<T>, H2Orm {
    * Write selected rows to csv file.
    *
    * @param toFile
-   * @param selectSql
+   * @param query
    * @return
    */
-  default File writeCsv(File toFile, String selectSql) {
-    getOrm().executeUpdate("call " + CsvWrite.builder(toFile).query(selectSql).build().getSql());
+  default File writeCsv(File toFile, String query) {
+    getOrm().executeUpdate("call " + CsvWrite.builder(toFile).query(query).build().getSql());
     return toFile;
-  }
-
-  default void scriptTableTo(File destFile, boolean includeDrop) {
-    String sql =
-        ScriptSql.builder()
-            .drop(includeDrop)
-            .to(destFile)
-            .addTable(getTableName())
-            .build()
-            .getSql();
-    getOrm().execute(sql);
-  }
-
-  default void scriptTableTo(File destFile, boolean includeDrop, String password) {
-    String sql =
-        ScriptSql.builder()
-            .drop(includeDrop)
-            .to(destFile)
-            .addTable(getTableName())
-            .scriptCompressionEncryption(
-                ScriptCompressionEncryption.defaultBuilder(password).build())
-            .build()
-            .getSql();
-    getOrm().execute(sql);
-  }
-
-  default int insertCsv(CsvRead csvRead) {
-    return getOrm().executeUpdate("insert into " + getTableName() + " select * from " + csvRead);
   }
 }
