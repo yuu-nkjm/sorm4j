06da9f4a04d6282b2fcab0c6d7892827081b2db0
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2023-12-15 10:12:48 +0900

    Add BackupSql

---
+++ sorm4j/src/main/java/org/nkjmlab/sorm4j/util/h2/commands/BackupSql.java
@@ -0,0 +1,35 @@
+package org.nkjmlab.sorm4j.util.h2.commands;
+
+import java.io.File;
+
+import org.nkjmlab.sorm4j.util.h2.internal.LiteralUtils;
+
+/** <a href="https://www.h2database.com/html/commands.html#backup">Commands</a> */
+public class BackupSql {
+  private final String sql;
+
+  public BackupSql(String sql) {
+    this.sql = sql;
+  }
+
+  public String getSql() {
+    return sql;
+  }
+
+  public static Builder builder(File file) {
+    return new Builder(file);
+  }
+
+  public static class Builder {
+
+    private File file;
+
+    public Builder(File file) {
+      this.file = file;
+    }
+
+    public BackupSql build() {
+      return new BackupSql("backup to " + LiteralUtils.wrapSingleQuote(file.getAbsolutePath()));
+    }
+  }
+}
