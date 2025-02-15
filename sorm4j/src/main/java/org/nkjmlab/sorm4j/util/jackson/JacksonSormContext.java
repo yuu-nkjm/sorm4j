7ea1a991938eb5307d00425457ea232363bf9db5
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2025-02-16 08:45:12 +0900

    Modify Jackson utils

--- sorm4j/src/main/java/org/nkjmlab/sorm4j/util/jackson/JacksonSormContext.java
+++ sorm4j/src/main/java/org/nkjmlab/sorm4j/util/jackson/JacksonSormContext.java
@@ -1,9 +1,17 @@
 package org.nkjmlab.sorm4j.util.jackson;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.nkjmlab.sorm4j.annotation.Experimental;
 import org.nkjmlab.sorm4j.context.DefaultColumnValueToJavaObjectConverters;
 import org.nkjmlab.sorm4j.context.DefaultSqlParametersSetter;
 import org.nkjmlab.sorm4j.context.SormContext;
+import org.nkjmlab.sorm4j.internal.util.ArrayUtils;
+import org.nkjmlab.sorm4j.util.datatype.OrmJsonColumnContainer;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Experimental
@@ -13,13 +21,46 @@ public class JacksonSormContext {
 
   public static SormContext.Builder builder(
       ObjectMapper objectMapper, Class<?>... ormJsonColumnContainerClasses) {
+
+    OrmJsonContainers ormJsonContainers = new OrmJsonContainers(ormJsonColumnContainerClasses);
+
     return SormContext.builder()
         .setColumnValueToJavaObjectConverters(
             new DefaultColumnValueToJavaObjectConverters(
-                new JacksonColumnValueToJavaObjectConverter(
-                    objectMapper, ormJsonColumnContainerClasses)))
+                new JacksonColumnValueToJavaObjectConverter(objectMapper, ormJsonContainers)))
         .setSqlParametersSetter(
             new DefaultSqlParametersSetter(
-                new JacksonSqlParameterSetter(objectMapper, ormJsonColumnContainerClasses)));
+                new JacksonSqlParameterSetter(objectMapper, ormJsonContainers)));
+  }
+
+  public static class OrmJsonContainers {
+    private final Map<Class<?>, Boolean> ormJsonContainer = new ConcurrentHashMap<>();
+
+    public OrmJsonContainers(Class<?>... ormJsonColumnContainerClasses) {
+      Arrays.stream(ormJsonColumnContainerClasses).forEach(c -> ormJsonContainer.put(c, true));
+    }
+
+    /**
+     * Determines whether the specified type should be processed as a JSON object.
+     *
+     * <p>This method checks whether the given type is a JSON container type. It handles:
+     *
+     * <ul>
+     *   <li>Lists: Any class that is assignable from {@link List} is considered a JSON object.
+     *   <li>Maps: Any class that is assignable from {@link Map} is considered a JSON object.
+     *   <li>Arrays: If the component type of the array is itself a JSON container or is annotated
+     *       with {@link OrmJsonColumnContainer}, it is considered a JSON object.
+     * </ul>
+     *
+     * @param toType the target type
+     * @return {@code true} if the type is considered a JSON object, otherwise {@code false}
+     */
+    public boolean isOrmJsonContainer(Class<?> toType) {
+      return List.class.isAssignableFrom(toType)
+          || Map.class.isAssignableFrom(toType)
+          || ormJsonContainer.getOrDefault(ArrayUtils.getInternalComponentType(toType), false)
+          || ArrayUtils.getInternalComponentType(toType).getAnnotation(OrmJsonColumnContainer.class)
+              != null;
+    }
   }
 }
--- sorm4j/src/main/java/org/nkjmlab/sorm4j/util/jackson/JacksonSqlParameterSetter.java
+++ sorm4j/src/main/java/org/nkjmlab/sorm4j/util/jackson/JacksonSqlParameterSetter.java
@@ -2,44 +2,29 @@ package org.nkjmlab.sorm4j.util.jackson;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+
 import org.nkjmlab.sorm4j.annotation.Experimental;
 import org.nkjmlab.sorm4j.context.SqlParameterSetter;
-import org.nkjmlab.sorm4j.internal.util.ArrayUtils;
 import org.nkjmlab.sorm4j.internal.util.Try;
-import org.nkjmlab.sorm4j.util.datatype.OrmJsonColumnContainer;
+import org.nkjmlab.sorm4j.util.jackson.JacksonSormContext.OrmJsonContainers;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Experimental
 public class JacksonSqlParameterSetter implements SqlParameterSetter {
   private final ObjectMapper objectMapper;
-  private final Map<Class<?>, Boolean> ormJsonContainer = new ConcurrentHashMap<>();
+  private final OrmJsonContainers ormJsonContainers;
 
-  public JacksonSqlParameterSetter(
-      ObjectMapper objectMapper, Class<?>... ormJsonColumnContainerClasses) {
+  public JacksonSqlParameterSetter(ObjectMapper objectMapper, OrmJsonContainers ormJsonContainers) {
     this.objectMapper = objectMapper;
-    Arrays.stream(ormJsonColumnContainerClasses).forEach(c -> ormJsonContainer.put(c, true));
-  }
-
-  private boolean isOrmJsonContainer(Class<?> type) {
-    return ormJsonContainer.computeIfAbsent(
-        type,
-        key ->
-            ArrayUtils.getInternalComponentType(type).getAnnotation(OrmJsonColumnContainer.class)
-                    != null
-                || List.class.isAssignableFrom(type)
-                || Map.class.isAssignableFrom(type)
-                || ormJsonContainer.getOrDefault(ArrayUtils.getInternalComponentType(type), false));
+    this.ormJsonContainers = ormJsonContainers;
   }
 
   @Override
   public boolean test(PreparedStatement stmt, int parameterIndex, Object parameter)
       throws SQLException {
-    return isOrmJsonContainer(parameter.getClass());
+    return ormJsonContainers.isOrmJsonContainer(parameter.getClass());
   }
 
   @Override
